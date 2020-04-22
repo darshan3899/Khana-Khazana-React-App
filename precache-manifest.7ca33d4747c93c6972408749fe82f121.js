e947f081589e36c91c0e9c1c4c2cc71c3864cbe0
darshan3899 <dmshah.3899@gmail.com>
2020-04-22 17:37:25 +0530

    Updates

---
+++ precache-manifest.7ca33d4747c93c6972408749fe82f121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d051a7cfdb6e9e22d194173c93b6a5d",
+    "url": "/Khana-Khazana-React-App/index.html"
+  },
+  {
+    "revision": "eadcf74b45137e383b73",
+    "url": "/Khana-Khazana-React-App/static/css/main.2e78fc5a.chunk.css"
+  },
+  {
+    "revision": "d8f07cb582dbfacc7153",
+    "url": "/Khana-Khazana-React-App/static/js/2.b743afb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Khana-Khazana-React-App/static/js/2.b743afb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadcf74b45137e383b73",
+    "url": "/Khana-Khazana-React-App/static/js/main.79a06c9d.chunk.js"
+  },
+  {
+    "revision": "0f4e0c97ab3d4ef080a5",
+    "url": "/Khana-Khazana-React-App/static/js/runtime-main.bef9c246.js"
+  }
+]);
